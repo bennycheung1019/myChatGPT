018ad63c1538f15c21b3a8cc2fa1e83156b3c769
Benny Cheung <daydreamer10191980@gmail.com>
2023-04-25 14:50:10 +0800

    no env

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's reponse
   
-  const response = await fetch('https://mychatgpt-o7de.onrender.com', {
+  const response = await fetch('https://mychatgpt-o7de.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
